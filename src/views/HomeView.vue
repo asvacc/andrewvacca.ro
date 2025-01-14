805e2b1203629fdae052707a1fe804f9414bfb45
Andrew <asvxyz@yahoo.com>
2025-01-13 21:15:50 -0500

    wip

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,15 @@
     <div class="grid items-center sm:gap-12 lg:gap-16 md:grid-cols-2 ">
       <Transition appear>
         <div>
-            <h1 class="text-lg sm:text-3xl lg:text-4xl">Andrew Vaccaro, Software Engineer</h1>
-            <p class="mt-6 text-5xl leading-[1] sm:leading-tight lg:leading-tight sm:text-6xl sm:mt-8 lg:mt-16 lg:text-7xl"><span
-                class="text-amber-400">Software Engineer</span> with an eye for design.</p>
+          <h1 class="text-lg sm:text-3xl lg:text-4xl">Andrew Vaccaro, Software Engineer</h1>
+          <p
+            class="mt-6 text-5xl leading-[1] sm:leading-tight lg:leading-tight sm:text-6xl sm:mt-8 lg:mt-16 lg:text-7xl">
+            <span class="text-amber-400">Software Engineer</span> with an eye for design.</p>
         </div>
       </Transition>
-      <img class="mt-8 sm:mt-0" src="https://picsum.photos/1200" />
+      <div class="md:min-h-[31.25rem] flex flex-col items-start justify-center">
+        <img class="mt-8 sm:mt-0" src="/me.webp" />
+      </div>
     </div>
 
     <div class="flex justify-center md:justify-start">
