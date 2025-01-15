6e08e7af6c6882c24d624c39a5a8d0fc993a8b7f
Andrew <asvxyz@yahoo.com>
2025-01-15 10:38:55 -0500

    Lazy load routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ContactView from '../views/ContactView.vue'
-import PortfolioView from '../views/PortfolioView.vue'
-import ResumeView from '../views/ResumeView.vue'
+
+const HomeView = () => import('../views/HomeView.vue')
+const ContactView = () => import('../views/ContactView.vue')
+const PortfolioView = () => import('../views/PortfolioView.vue')
+const ResumeView = () => import('../views/ResumeView.vue')
+
 
 const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
